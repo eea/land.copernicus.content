12e6071b817d057db327dbe0b514ad84a32bf4d1
Olimpiu Rob <olimpiu.rob@gmail.com>
2013-05-29 20:20:57 +0200

    Refs #14665; added a migration view to cleanup scales after thumbnail size redefinition

---
+++ land/copernicus/content/browser/migration.py
@@ -0,0 +1,26 @@
+import logging
+
+logger = logging.getLogger('land.copernicus.content')
+
+class FixImageScales(object):
+    """fix image scales for the image field
+    """
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    def __call__(self):
+        logger.info("Starting to convert scales")
+        cat = self.context.portal_catalog
+        for _type in ['LandItem', 'LandSection']:
+            brains = cat(portal_type=_type)
+            for brain in brains:
+                obj = brain.getObject()
+                field = obj.getField("image")
+                field.removeScales(obj)
+                field.createScales(obj)
+                logger.info("Converted scales for %s", obj.absolute_url())
+
+        logger.info("Finished converting scales")
+        
