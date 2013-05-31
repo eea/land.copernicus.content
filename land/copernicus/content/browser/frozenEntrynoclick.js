20e24aa8524675af81307ddd22de88c1b2d70499
Olimpiu Rob <olimpiu.rob@gmail.com>
2013-05-31 18:45:39 +0300

    Add separate javascript for marking News site-action as frozen

--- land/copernicus/content/browser/frozenEntrynoclick.js
+++ land/copernicus/content/browser/frozenEntrynoclick.js
@@ -1,5 +1,4 @@
 jQuery(document).ready(function() {
-    jQuery('#siteaction-news').addClass("frozenEntry");
     jQuery('.frozenEntry a').bind('click', function(event){
         return false;
     });
---
+++ land/copernicus/content/browser/markFrozen.js
@@ -0,0 +1,3 @@
+jQuery(document).ready(function() {
+    jQuery('#siteaction-news').addClass("frozenEntry");
+});
