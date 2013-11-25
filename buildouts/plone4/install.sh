72f1b4a696ccbc9f43af54f98ea3bb74453c4a93
Olimpiu Rob <olimpiu.rob@gmail.com>
2013-11-25 17:36:03 +0200

    Updated jenkins buildout files

--- buildouts/plone4/install.sh
+++ buildouts/plone4/install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-wget -O https://svn.eionet.europa.eu/repositories/Zope/trunk/www.eea.europa.eu/trunk/install.sh
+wget -O bootstrap.sh https://raw.github.com/olimpiurob/eea.plonebuildout.core/master/install.sh
 chmod u+x bootstrap.sh
 ./bootstrap.sh
