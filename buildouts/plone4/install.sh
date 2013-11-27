843903198d41e36cc469ecef75b608531f2f03a9
Olimpiu Rob <olimpiu.rob@gmail.com>
2013-11-27 09:50:03 +0200

    Changed install.sh path

--- buildouts/plone4/install.sh
+++ buildouts/plone4/install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-wget -O bootstrap.sh https://raw.github.com/eea/eea.plonebuildout.core/master/install.sh
+wget -O bootstrap.sh https://raw.github.com/olimpiurob/eea.plonebuildout.core/master/install.sh
 chmod u+x bootstrap.sh
 ./bootstrap.sh
