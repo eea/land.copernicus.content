baaccaf37191a210344707a95b6c99476b71ab82
Olimpiu Rob <olimpiu.rob@gmail.com>
2015-03-27 17:45:23 +0200

    Refs 24190; Added LDAP specific properties to local users

--- land/copernicus/content/adapter.py
+++ land/copernicus/content/adapter.py
@@ -28,6 +28,55 @@ class EnhancedUserDataPanelAdapter(UserDataPanelAdapter):
         return self.context.setMemberProperties({'institutional_domain': institutional_domains})
     institutional_domain = property(get_institutional_domain, set_institutional_domain)
 
+    def get_reason(self):
+        return self.context.getProperty('reason', '')
+
+    def set_reason(self, value):
+        return self.context.setMemberProperties({'reason': value})
+    reason = property(get_reason, set_reason)
+
+    def get_job_title(self):
+        return self.context.getProperty('job_title', '')
+
+    def set_job_title(self, value):
+        return self.context.setMemberProperties({'job_title': value})
+    job_title = property(get_job_title, set_job_title)
+
+    def get_postal_address(self):
+        return self.context.getProperty('postal_address', '')
+
+    def set_postal_address(self, value):
+        return self.context.setMemberProperties({'postal_address': value})
+    postal_address = property(get_postal_address, set_postal_address)
+
+    def get_telephone(self):
+        return self.context.getProperty('telephone', '')
+
+    def set_telephone(self, value):
+        return self.context.setMemberProperties({'telephone': value})
+    telephone = property(get_telephone, set_telephone)
+
+    def get_mobile(self):
+        return self.context.getProperty('mobile', '')
+
+    def set_mobile(self, value):
+        return self.context.setMemberProperties({'mobile': value})
+    mobile = property(get_mobile, set_mobile)
+
+    def get_fax(self):
+        return self.context.getProperty('fax', '')
+
+    def set_fax(self, value):
+        return self.context.setMemberProperties({'fax': value})
+    fax = property(get_fax, set_fax)
+
+    def get_organisation(self):
+        return self.context.getProperty('organisation', '')
+
+    def set_organisation(self, value):
+        return self.context.setMemberProperties({'organisation': value})
+    organisation = property(get_organisation, set_organisation)
+
     def get_disclaimer(self):
         return self.context.getProperty('disclaimer', '')
 
--- land/copernicus/content/userdataschema.py
+++ land/copernicus/content/userdataschema.py
@@ -4,12 +4,7 @@ from zope import schema
 from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 from zope.interface import implements
 from land.copernicus.content.config import EEAMessageFactory as _
-
-
-gender_options = SimpleVocabulary([
-    SimpleTerm(value='Male', title=_(u'Male')),
-    SimpleTerm(value='Female', title=_(u'Female')),
-    ])
+import re
 
 thematic_domain_options = SimpleVocabulary([
     SimpleTerm(value='Environment', title=_(u'Environment')),
@@ -38,6 +33,13 @@ def validateAccept(value):
     return True
 
 
+def validate_phone(value):
+    phone_re = re.compile(r'(\d{3})\D*(\d{3})\D*(\d{4})\D*(\d*)$', re.VERBOSE)
+    if phone_re.match(value):
+        return True
+    return False
+
+
 class UserDataSchemaProvider(object):
     implements(IUserDataSchemaProvider)
 
@@ -52,12 +54,66 @@ class IEnhancedUserDataSchema(IUserDataSchema):
     extra fields.
     """
     thematic_domain = schema.List(
-        title=u"Thematic domain of usage of the data",
+        title=_(u'label_thematic_domain',
+                default=u'Thematic domain of usage of the data'),
         value_type=schema.Choice(vocabulary=thematic_domain_options))
 
     institutional_domain = schema.List(
-        title=u"Institutional domain of usage of the data",
+        title=_(u'label_institutional_domain',
+                default=u'Institutional domain of usage of the data'),
         value_type=schema.Choice(vocabulary=institutional_domain_options))
+
+    reason = schema.TextLine(
+        title=_(u'label_reason', default=u'Reason to create the account'),
+        description=_(u'help_reason',
+                      default=u'Fill in the reason for account creation'),
+        required=False,
+        )
+
+    job_title = schema.TextLine(
+        title=_(u'label_job_title', default=u'Job title'),
+        description=_(u'help_job_title',
+                      default=u'Fill in the job title'),
+        required=False,
+        )
+
+    postal_address = schema.Text(
+        title=_(u'label_postal_address', default=u'Postal address'),
+        description=_(u'help_postal_address',
+                      default=u'Fill in the postal address'),
+        required=False,
+        )
+
+    telephone = schema.ASCIILine(
+        title=_(u'label_telephone', default=u'Telephone number'),
+        description=_(u'help_telephone',
+                      default=u'Fill in the telephone number'),
+        required=False,
+        constraint=validate_phone
+        )
+
+    mobile = schema.ASCIILine(
+        title=_(u'label_mobile', default=u'Mobile telephone number'),
+        description=_(u'help_mobile',
+                      default=u'Fill in the mobile telephone number'),
+        required=False,
+        constraint=validate_phone
+        )
+
+    fax = schema.ASCIILine(
+        title=_(u'label_fax', default=u'Fax number'),
+        description=_(u'help_fax',
+                      default=u'Fill in the fax number'),
+        required=False,
+        constraint=validate_phone
+        )
+
+    organisation = schema.TextLine(
+        title=_(u'label_organisation', default=u'Organisation'),
+        description=_(u'help_organisation',
+                      default=u'Fill in the organisation'),
+        required=False,
+        )
 
     disclaimer = schema.Bool(
         title=_(u'label_disclaimer', default=u'Accept terms of use'),
