bb3c326025dc6fc52559ef5aa1d90db18b3ebd56
Tiberiu Ichim <tiberiu@pixelblaster.ro>
2015-04-15 11:44:37 +0300

    Remove init code for LandProduct

--- land/copernicus/content/content/__init__.py
+++ land/copernicus/content/content/__init__.py
@@ -4,7 +4,6 @@ from Products.ATContentTypes.content.base import registerATCT
 from land.copernicus.content.config import PACKAGE
 from land.copernicus.content.content import landfile
 from land.copernicus.content.content import landitem
-from land.copernicus.content.content import landproduct
 from land.copernicus.content.content import landsection
 
 def register():
@@ -12,5 +11,4 @@ def register():
     """
     registerATCT(landsection.LandSection, PACKAGE)
     registerATCT(landitem.LandItem, PACKAGE)
-    registerATCT(landproduct.LandProduct, PACKAGE)
     registerATCT(landfile.LandFile, PACKAGE)
