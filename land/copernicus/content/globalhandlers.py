8d3e84876c829b3239c42746b5a4db2306c00440
Ghiță Bizău <ghitabizau27@gmail.com>
2015-05-04 14:56:13 +0300

    Refs #25172 Copernicus; Fixed user fullname in register.

--- land/copernicus/content/globalhandlers.py
+++ land/copernicus/content/globalhandlers.py
@@ -1,12 +1,14 @@
-def autofillFullname(context):
+from zope.component.hooks import getSite
+
+
+def autofillFullname(principal, event):
     """ Because we splitted user fullname in first_name and last_name on
         register, fullname was omited from schema. But we autofill this
         on register event.
     """
-    plone_user = context.principal
-    properties = plone_user._propertysheets.get('mutable_properties')
-    first_name = properties.getProperty('first_name')
-    last_name = properties.getProperty('last_name')
+    site = getSite()
+    first_name = site.REQUEST.form.get('form.first_name')
+    last_name = site.REQUEST.form.get('form.last_name')
     auto_fullname = first_name + " " + last_name
-    properties.setProperty(plone_user, 'fullname', auto_fullname)
-    # [TODO] Fix first_name and last_name to have the real data in fields.
+    properties = principal._propertysheets.get('mutable_properties')
+    properties.setProperty(principal, 'fullname', auto_fullname)
