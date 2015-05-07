6391f5e5d19c1c33fb8d7c2270ba9a67cfef638d
Ghiță Bizău <ghitabizau27@gmail.com>
2015-05-07 14:08:20 +0300

    Refs #25172 Copernicus; Fixed bug for adduser command.

--- land/copernicus/content/globalhandlers.py
+++ land/copernicus/content/globalhandlers.py
@@ -7,8 +7,9 @@ def autofillFullname(principal, event):
         on register event.
     """
     site = getSite()
-    first_name = site.REQUEST.form.get('form.first_name')
-    last_name = site.REQUEST.form.get('form.last_name')
-    auto_fullname = first_name + " " + last_name
-    properties = principal._propertysheets.get('mutable_properties')
-    properties.setProperty(principal, 'fullname', auto_fullname)
+    if site is not None:
+        first_name = site.REQUEST.form.get('form.first_name')
+        last_name = site.REQUEST.form.get('form.last_name')
+        auto_fullname = first_name + " " + last_name
+        properties = principal._propertysheets.get('mutable_properties')
+        properties.setProperty(principal, 'fullname', auto_fullname)
