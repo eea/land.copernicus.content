69be973a1e9d6509085c3459f6fc073e641d5fe5
Ghita Bizau <ghitabizau27@gmail.com>
2015-05-19 15:38:13 +0300

    Refs #25742 Copernicus; Implemented error for broken land file url.

--- land/copernicus/content/browser/views.py
+++ land/copernicus/content/browser/views.py
@@ -1,6 +1,7 @@
 from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone.PloneBatch import Batch
 from Products.Five.browser import BrowserView
+import urllib2
 from zope.component.hooks import getSite
 from zope.component import getMultiAdapter
 
@@ -45,23 +46,58 @@ class GoPDB(BrowserView):
         return "done"
 
 
+def remoteUrl_exists(remoteUrl):
+    try:
+        urllib2.urlopen(remoteUrl)
+        return True
+    except urllib2.HTTPError, e:
+        print(e.code)
+        return False
+    except urllib2.URLError, e:
+        print(e.args)
+        return False
+
+
 class RedirectDownloadUrl(BrowserView):
     """ Redirect to download url for a LandItem if logged in """
+
+    def url_login_with_params(self):
+        """ Returns the login url with params in came_from url """
+
+        auto_selected = self.request.form.get('selected', None)
+        portal_state = getMultiAdapter(
+            (self.context, self.request), name=u'plone_portal_state')
+        root_url = portal_state.portal_url()
+        land_item_url = self.context.aq_parent.absolute_url()
+        login_url = root_url + "/login" + "?came_from=" + land_item_url + \
+            "?fieldsetlegend-download=True"
+
+        if auto_selected:
+            login_url += "-selected-" + auto_selected
+
+        return login_url
+
+    def url_missing_file(self):
+        """ Returns the url for case: file missing, page not found """
+
+        land_item_url = self.context.aq_parent.absolute_url()
+        open_download_tab_param = '?fieldsetlegend-download=true'
+        land_file_title = self.context.title
+        error_param = '-error-not-found-' + land_file_title
+        error_url = land_item_url + open_download_tab_param + error_param
+
+        return error_url
+
     def __call__(self):
         is_anonymous = \
             bool(getToolByName(
                 getSite(), 'portal_membership').isAnonymousUser())
 
         if is_anonymous:
-            auto_selected = self.request.form.get('selected', None)
-            portal_state = getMultiAdapter(
-                (self.context, self.request), name=u'plone_portal_state')
-            root_url = portal_state.portal_url()
-            land_item_url = self.context.aq_parent.absolute_url()
-            login_url = root_url + "/login" + "?came_from=" + land_item_url + \
-                "?fieldsetlegend-download=true"
-            if auto_selected:
-                login_url += "-selected-" + auto_selected
-            return self.request.response.redirect(login_url)
+            return self.request.response.redirect(self.url_login_with_params())
         else:
-            return self.request.response.redirect(self.context.remoteUrl)
+            remoteUrl = self.context.remoteUrl
+            if remoteUrl_exists(remoteUrl):
+                return self.request.response.redirect(remoteUrl)
+            else:
+                return self.request.response.redirect(self.url_missing_file())
