341ed8f243bca535f38ff2a1ed928d899181ef39
Ghita Bizau <ghitabizau27@gmail.com>
2015-05-25 10:50:50 +0300

    Refs #25807 Copernicus; Solved force update profile if missing info about domains.

--- land/copernicus/content/globalhandlers.py
+++ land/copernicus/content/globalhandlers.py
@@ -1,4 +1,7 @@
 from zope.component.hooks import getSite
+from Products.CMFCore.utils import getToolByName
+from zope.globalrequest import getRequest
+from Products.statusmessages.interfaces import IStatusMessage
 
 
 def autofillFullname(principal, event):
@@ -19,5 +22,25 @@ def forceUpdateProfile(principal, event):
     """ Redirect to edit profile if user has not completed info about
         thematic and institutional domains
     """
-    # import pdb; pdb.set_trace( )
-    # [TODO] WIP here.
+    site = getSite()
+    if site is not None:
+        membership = getToolByName(site, 'portal_membership')
+        authenticated_user = membership.getAuthenticatedMember()
+        t_d = authenticated_user.getProperty('thematic_domain', '')
+        i_d = authenticated_user.getProperty('institutional_domain', '')
+
+        if t_d == '' or i_d == '':
+            request = getRequest()
+
+            messages = IStatusMessage(request)
+            messages.add(
+                u"Please complete your profile adding your Professional " +
+                "thematic domain and Institutional domain.", type=u"info")
+
+            if request.get('came_from', None):
+                request['came_from'] = ''
+                request.form['came_from'] = ''
+                request.form['next'] = ''
+
+            edit_profile_url = site.portal_url() + '/@@personal-information'
+            request.RESPONSE.redirect(edit_profile_url)
