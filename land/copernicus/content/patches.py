205099873a88da33769a722e76b55c34928b063b
Ghita Bizau <ghitabizau27@gmail.com>
2015-07-01 18:20:52 +0300

    Refs #26806 Copernicus; Fixed missing add menu and comments enabled.

---
+++ land/copernicus/content/patches.py
@@ -0,0 +1,20 @@
+from Acquisition import aq_inner
+from plone.app.discussion.browser.conversation import ConversationView
+old_enabled = ConversationView.enabled
+
+
+def enabled(self):
+    parent = aq_inner(self.__parent__)
+    if parent.portal_type in [
+        'HelpCenterDefinition', 'HelpCenterErrorReference', 'HelpCenterFAQ',
+        'HelpCenterFAQFolder', 'HelpCenterGlossary', 'HelpCenter',
+        'HelpCenterHowTo', 'HelpCenterHowToFolder',
+        'HelpCenterInstructionalVideo', 'HelpCenterInstructionalVideoFolder',
+        'HelpCenterKnowledgeBase', 'HelpCenterLeafPage', 'HelpCenterLink',
+        'HelpCenterLinkFolder', 'HelpCenterReferenceManual',
+        'HelpCenterReferenceManualFolder', 'HelpCenterReferenceManualPage',
+        'HelpCenterReferenceManualSection', 'HelpCenterTutorial',
+        'HelpCenterTutorialFolder', 'HelpCenterTutorialPage'
+        ]:
+        return True
+    return old_enabled(self)
