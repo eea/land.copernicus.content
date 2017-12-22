b296ee6c9935871aa3e162a75cff174639fe24d9
Ghiță Bizău <ghita_bizau@yahoo.com>
2017-12-22 12:27:26 +0200

    [refs #90603] Re-enable geotags field in landitem.

--- land/copernicus/content/content/schema.py
+++ land/copernicus/content/content/schema.py
@@ -3,8 +3,8 @@
 from AccessControl import ClassSecurityInfo
 from DateTime import DateTime
 from eea.forms.browser.app.temporal_coverage import grouped_coverage
-# from eea.geotags.field import GeotagsLinesField
-# from eea.geotags.widget import GeotagsWidget
+from eea.geotags.field import GeotagsLinesField
+from eea.geotags.widget import GeotagsWidget
 from land.copernicus.content.content.vocabulary import COUNTRIES_DICTIONARY_ID
 from Products.Archetypes import atapi
 from Products.Archetypes.atapi import BooleanField
@@ -668,20 +668,19 @@ PRODUCT_SCHEMA = Schema((
         default_output_type="text/x-html-safe",
     ),
     # CATEGORIZATION ==========================================================
-    # [refs #90603] TODO WIP here.
-    # GeotagsLinesField(
-    #     'geographicCoverageGT',
-    #     schemata='categorization',
-    #     required=True,
-    #     widget=GeotagsWidget(
-    #         label='Geographic coverage',
-    #         description="Type in here the exact geographic names/places "\
-    #         "that are covered by the data. Add Countries names only "\
-    #         "if the data displayed is really about the entire country. "\
-    #         "Example of locations/places are lakes, rivers, cities, "\
-    #         "marine areas, glaciers, bioregions like alpine region etc."
-    #     )
-    # ),
+    GeotagsLinesField(
+        'geographicCoverageGT',
+        schemata='categorization',
+        required=True,
+        widget=GeotagsWidget(
+            label='Geographic coverage',
+            description="Type in here the exact geographic names/places "\
+            "that are covered by the data. Add Countries names only "\
+            "if the data displayed is really about the entire country. "\
+            "Example of locations/places are lakes, rivers, cities, "\
+            "marine areas, glaciers, bioregions like alpine region etc."
+        )
+    ),
 ))
 
 
