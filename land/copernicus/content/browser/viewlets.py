291f55f2934964767336bad1300c2f58049c3a1f
Irina Botez <irina.botez@eaudeweb.ro>
2018-01-23 11:58:16 +0200

    Added condition for header viewlets
    Thew viewlet will be rendered only if 'SENTRY_DSN' exists as an
    environment variable and has a value

--- land/copernicus/content/browser/viewlets.py
+++ land/copernicus/content/browser/viewlets.py
@@ -9,6 +9,11 @@ class SentryViewlet(ViewletBase):
     def render(self):
         return super(ViewletBase, self).render()
 
+    def has_dsn(self):
+        if 'SENTRY_DSN' in os.environ.keys() and os.environ['SENTRY_DSN'] != '':
+            return True
+        return False
+
     def get_dsn(self):
         dsn = os.environ["SENTRY_DSN"]
         passwd = re.search(r'.*(:.*?)@.*', dsn).group(1)
