6b0f850b811740e8a934203bd91dd05a0943b006
Ghiță Bizău <ghita_bizau@yahoo.com>
2018-02-26 19:39:17 +0200

    [refs #90603] WIP migrate Geographic Coverage field contents.

--- land/copernicus/content/upgrades/evolve32.py
+++ land/copernicus/content/upgrades/evolve32.py
@@ -1,11 +1,27 @@
-import logging
 from plone import api
+import logging
+import transaction
 
 
 logger = logging.getLogger('land.copernicus.content')
 
 
 def do_migration(landitem):
+    # TODO Save old field value in new one in geotags requested format
+    # tool = api.portal.get_tool('portal_languages')
+    # countries = dict(tool.listAvailableCountries())
+    # if "test-landitem" in landitem.absolute_url():
+    #     locations = [
+    #         countries.get(t, t) for t in landitem.getGeographicCoverage()
+    #     ]
+    #
+    #     import pdb; pdb.set_trace()
+    #     locations = ['Bulgaria', 'Romania']
+    #     # landitem.geographicCoverageGT = locations
+    #     landitem.getField('geographicCoverageGT').set(landitem, locations)
+    #     landitem.reindexObject()
+    #     transaction.commit()
+
     print "WIP getGeographicCoverage -> getGeographicCoverageGT"
 
 
