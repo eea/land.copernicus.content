9a9ef804b5487de2be9b8ad649963f172a76f201
David Bătrânu <david.batranu@eaudeweb.ro>
2018-03-05 18:58:36 +0200

    [refs #93407] Update land file sizes.

---
+++ land/copernicus/content/upgrades/evolve33.py
@@ -0,0 +1,36 @@
+import logging
+from plone import api
+from land.copernicus.content.content.api import get_filesize
+from land.copernicus.content.content.landfile import LandFileStore
+
+
+logger = logging.getLogger('land.copernicus.content')
+
+
+def run(_):
+    catalog = api.portal.get_tool(name='portal_catalog')
+    land_items = [b.getObject() for b in catalog(portal_type='LandItem')]
+    total_li = len(land_items)
+
+    for idx_li, land_item in enumerate(land_items, start=1):
+        url_land_item = land_item.absolute_url(1)
+        logger.info('[%s/%s] Updating %s', idx_li, total_li, url_land_item)
+        store = land_item.landfiles # type: LandFileStore
+        land_files = store.get_all()
+        total_lf = len(land_files)
+
+        for idx_lf, lf in enumerate(land_files, start=1):
+            short_name = lf.shortname
+            logger.info('[%s/%s] Updating %s', idx_lf, total_lf, short_name)
+            new_file_size = ''
+            try:
+                new_file_size = get_filesize(lf.remoteUrl)
+            except OSError:
+                logger.warn('Missing path for land file: %s!', lf.remoteUrl)
+
+            if new_file_size:
+                logger.info(
+                    'Updating file size for: %s: %s (%s) [%s]',
+                    url_land_item, short_name, lf.remoteUrl, new_file_size)
+                lf._fileSize = new_file_size
+        logger.info('Done %s', url_land_item)
