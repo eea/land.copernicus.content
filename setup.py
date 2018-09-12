0884039129c8fa599cf0477f2942794e04090590
Ghiță Bizău <ghita_bizau@yahoo.com>
2018-09-12 12:31:14 +0300

    [refs #98066] Install requires: add collective.recaptcha.

--- setup.py
+++ setup.py
@@ -40,7 +40,8 @@ setup(
         'plone.app.async',
         'five.pt',
         'plone.stringinterp==1.0.14',  # needed for IContextWrapper in async
-        'eea.rdfmarshaller>=10.5'
+        'eea.rdfmarshaller>=10.5',
+        'collective.recaptcha'
     ],
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
