9b1aa9cea8d36b6d0050e5813fe3baae9edb7253
Ghiță Bizău <ghita_bizau@yahoo.com>
2018-10-05 15:41:35 +0300

    [refs #97529] Fix layers priority: ILandCopernicusContentLayer / IMeetingLayer in order to make register override working.

--- land/copernicus/content/interfaces.py
+++ land/copernicus/content/interfaces.py
@@ -1,5 +1,6 @@
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+from eea.meeting.interfaces.layer import IMeetingLayer
 
 
-class ILandCopernicusContentLayer(IDefaultBrowserLayer):
+class ILandCopernicusContentLayer(IDefaultBrowserLayer, IMeetingLayer):
     """ Marker interface that defines a browser layer. """
