29012dacc2a793bedf1c5e8b1d50c0f57c01f6ca
Ghiță Bizău <ghita_bizau@yahoo.com>
2018-11-06 17:48:24 +0200

    [refs #99958] Subscribers list: add excel.js customization.

---
+++ land/copernicus/content/skins/land_content_templates/eea.meeting.browser.static.excel.js
@@ -0,0 +1,21 @@
+window.saveFile = function saveFile (selector, title, json_opts) {
+  alert("CUSTOM");
+    debugger;
+    var table = $(selector).tableToJSON(json_opts);
+
+
+    if(selector == '#subscribers'){
+        table.forEach(function(item){
+            delete item['Address'];
+            delete item['Reimbursed'];
+            delete item['State'];
+            delete item['User Name'];
+            delete item['Visa'];
+            delete item['Role'];
+        });
+    }
+
+    var opts = [{sheetid:'Sheet 1',header:true}];
+    var res = alasql('SELECT * INTO XLSX("Export_'+title+'.xls",?) FROM ?',
+                     [opts,[table]]);
+}
