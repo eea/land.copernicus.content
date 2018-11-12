f0febe246852eaab719f25a407a041ea3a5760e9
Ghiță Bizău <ghita_bizau@yahoo.com>
2018-11-12 16:50:14 +0200

    [refs #99957] Meeting register: prefill organisation, position, country, address, email, phone.

--- land/copernicus/content/browser/overrides/meeting.py
+++ land/copernicus/content/browser/overrides/meeting.py
@@ -261,11 +261,11 @@ class Register(views.Register):
             'fields': {
                 "first_name": user.getProperty('first_name', uid),
                 "last_name": user.getProperty('last_name', uid),
-                "organisation": "Organisation",
-                "position": "Position",
-                "country": "Country",
-                "address": "City",
-                "email": "Email address",
-                "phone_no": "123123123"
+                "organisation": user.getProperty('organisation', uid),
+                "position": user.getProperty('position', uid),
+                "country": user.getProperty('country', uid),
+                "address": user.getProperty('address', uid),
+                "email": user.getProperty('email', uid),
+                "phone_no": user.getProperty('phone_no', uid)
             }
         }
