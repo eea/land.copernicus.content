1ab58aff7d148cadaf040dfc304a14fe4a1c770e
Ghiță Bizău <ghita_bizau@yahoo.com>
2018-11-14 16:24:42 +0200

    [refs #99957] Subscriber edit: get rid of unused fields.

--- land/copernicus/content/eea_meeting.py
+++ land/copernicus/content/eea_meeting.py
@@ -3,9 +3,9 @@ from eea.meeting import _
 from eea.meeting.interfaces.util import validate_email
 from eea.meeting.interfaces.util import validate_userid
 from plone.app.textfield import RichText
-from plone.autoform import directives
+# from plone.autoform import directives
 from plone.namedfile.field import NamedBlobFile
-from z3c.form.browser.radio import RadioFieldWidget
+# from z3c.form.browser.radio import RadioFieldWidget
 from zope import schema
 from zope.interface import Interface
 from zope.interface import invariant, Invalid
@@ -152,17 +152,11 @@ class ISubscriber(Interface):
         constraint=validate_email
     )
 
-    directives.widget(reimbursed=RadioFieldWidget)
-    reimbursed = schema.Bool(
-        title=_(u"Reimbursed participation"),
-        required=True
-    )
-
-    directives.widget(visa=RadioFieldWidget)
-    visa = schema.Bool(
-        title=_(u"I need visa support letter"),
-        required=True
-    )
+    # directives.widget(reimbursed=RadioFieldWidget)
+    # reimbursed = schema.Bool(
+    #     title=_(u"Reimbursed participation"),
+    #     required=True
+    # )
 
     role = schema.Choice(
         title=_(u"Role"),
