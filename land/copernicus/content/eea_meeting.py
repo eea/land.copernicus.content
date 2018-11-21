a47bc779c13992730c88711d8e242b87d7cc91fc
Ghiță Bizău <ghita_bizau@yahoo.com>
2018-11-21 15:07:01 +0200

    [refs #99957] Subscriber edit: fix bug for changing bith date and valid id date.

--- land/copernicus/content/eea_meeting.py
+++ land/copernicus/content/eea_meeting.py
@@ -3,9 +3,7 @@ from eea.meeting import _
 from eea.meeting.interfaces.util import validate_email
 from eea.meeting.interfaces.util import validate_userid
 from plone.app.textfield import RichText
-# from plone.autoform import directives
 from plone.namedfile.field import NamedBlobFile
-# from z3c.form.browser.radio import RadioFieldWidget
 from zope import schema
 from zope.interface import Interface
 from zope.interface import invariant, Invalid
@@ -13,6 +11,7 @@ from zope.interface import provider
 from zope.schema.interfaces import IVocabularyFactory
 from zope.schema.vocabulary import SimpleTerm
 from zope.schema.vocabulary import SimpleVocabulary
+import datetime
 
 
 meeting_types = SimpleVocabulary(
@@ -172,6 +171,9 @@ class ISubscriber(Interface):
     date_of_birth = schema.Date(
         title=_(u"DATE OF BIRTH"),
         required=False,
+        min=datetime.date(1900, 1, 1),
+        max=datetime.date(2050, 1, 1),
+
     )
 
     nationality = schema.TextLine(
@@ -187,6 +189,8 @@ class ISubscriber(Interface):
     id_valid_date = schema.Date(
         title=_(u"ID VALID DATE"),
         required=False,
+        min=datetime.date(1900, 1, 1),
+        max=datetime.date(2050, 1, 1),
     )
 
     parking = schema.TextLine(
