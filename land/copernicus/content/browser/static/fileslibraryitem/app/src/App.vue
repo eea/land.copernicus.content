d9991fc6f42a0db7a66b1779203c8aae478eff65
Ghiță Bizău <ghita_bizau@yahoo.com>
2019-01-10 16:01:09 +0200

    [refs #99877] Files library admin: special cells for file upload.

--- land/copernicus/content/browser/static/fileslibraryitem/app/src/App.vue
+++ land/copernicus/content/browser/static/fileslibraryitem/app/src/App.vue
@@ -30,6 +30,7 @@
         <tr v-for="(row, index_row) in rows" :key="index_row">
           <td v-for="(column, index_col) in columns" :key="column.id">
             <editable :content="rows[index_row][index_col].text" v-on:update="update_row($event, index_row, index_col)"></editable>
+            <p :data-uploader-id="index_col" v-if="is_file_col(columns[index_col].text)">file uploader placeholder</p>
           <td>
             <i class="fa fa-arrow-up fa-2x dta-btn move-row-up" v-on:click="move_row_up(index_row)" title="Move row up"></i>
             <i class="fa fa-arrow-down fa-2x dta-btn move-row-down" v-on:click="move_row_down(index_row)" title="Move row down"></i>
@@ -193,8 +194,16 @@ export default {
       }
     },
 
+    is_url_col: function(col_name) {
+      return col_name == 'URL';
+    },
+
+    is_file_col: function(col_name) {
+      return col_name == 'File';
+    },
+
     is_editable_col: function(col_name) {
-      return col_name !== "URL" && col_name !== 'File';
+      return !(this.is_url_col(col_name) || this.is_file_col(col_name));
     },
 
     generate_id: function() {
