369292ce9c3c2a9495f87bd1cc5c8dc6f9d5b29a
Ghiță Bizău <ghita_bizau@yahoo.com>
2019-01-15 16:32:58 +0200

    [refs #99877] Files library view: create file on upload.

--- land/copernicus/content/browser/fileslibraryitem.py
+++ land/copernicus/content/browser/fileslibraryitem.py
@@ -1,8 +1,14 @@
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone import api
+from plone.namedfile.file import NamedBlobFile
 import json
 import transaction
+
+
+def getvalue(value):
+    if value:
+        return value.strip()
 
 
 class FilesLibraryItemView(BrowserView):
@@ -32,11 +38,35 @@ class FilesLibraryItemAdminView(BrowserView):
 
         return json.dumps(files_ids)
 
+    def _create_file(self, uploaded_file):
+        kw = {}
+
+        if uploaded_file and uploaded_file.filename:
+            kw['name'] = NamedBlobFile(
+                filename=uploaded_file.filename.decode('utf-8'),
+                data=uploaded_file.read()
+            )
+
+        item = api.content.create(
+            container=self.context,
+            type="File",
+            id=getvalue(uploaded_file.filename),
+            title=getvalue(uploaded_file.filename),
+            safe_id=False,
+            **kw)
+
+        return item
+
     def __call__(self):
         if self.request.method == 'POST':
             data = self.request.form.get("exported-json", None)
             if data:
                 self.context.json_data = data
                 transaction.commit()
+
+            uploaded_file = self.request.form.get("file", None)
+            if uploaded_file:
+                result = self._create_file(uploaded_file)
+                print result.title
 
         return self.render()
--- land/copernicus/content/browser/static/fileslibraryitem/app/src/components/filesupload.vue
+++ land/copernicus/content/browser/static/fileslibraryitem/app/src/components/filesupload.vue
@@ -1,13 +1,19 @@
 <template>
-  <div class="container">
-    <label>File</label>
-    <input type="file" id="file" ref="file" v-on:change="handle_file_upload()"/>
-    <button v-on:click="submit_file()">Upload</button>
+  <div class="form-container">
+    <form class="files-upload-form" ref="files_upload_form"
+          action="./admin_files_library" method="post" enctype="multipart/form-data">
+        <label>File</label>
+        <input type="file" id="file" ref="file" name="file" v-on:change="handle_file_upload()"/>
+        <button v-on:click="submit_file()">Upload</button>
+    </form>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
+
   name: 'filesupload',
   data(){
     return {
@@ -19,18 +25,19 @@ export default {
       this.file = this.$refs.file.files[0];
     },
     submit_file() {
-      let form_data = new FormData();
-      form_data.append('file', this.file);
-
-      axios.post('/single-file', form_data, {
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        }
-      }).then(function(){
-        console.log('SUCCESS!!');
-      }).catch(function(){
-        console.log('FAILURE!!');
-      });
+      this.$refs.files_upload_form.submit();
+      // let form_data = new FormData();
+      // form_data.append('file', this.file);
+      //
+      // axios.post('./admin_files_library', form_data, {
+      //   headers: {
+      //     'Content-Type': 'multipart/form-data'
+      //   }
+      // }).then(function(){
+      //   console.log('SUCCESS!!');
+      // }).catch(function(){
+      //   console.log('FAILURE!!');
+      // });
     }
   }
 }
