e0e96644f7e032cd9b073a5643d173eba88daac4
Ghiță Bizău <ghita_bizau@yahoo.com>
2019-01-29 16:41:47 +0200

    [refs #101742] Users email notifications: add encode / decode features.

--- land/copernicus/content/browser/users_email_notifications.py
+++ land/copernicus/content/browser/users_email_notifications.py
@@ -1,12 +1,40 @@
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 import logging
+import base64
 
 logger = logging.getLogger('land.copernicus.content')
 
 
+SECRET_KEY_DEMO = "aaabbbccc"  # TODO Set a key as env var
+
+
+def encode(key, clear):
+    enc = []
+    for i in range(len(clear)):
+        key_c = key[i % len(key)]
+        enc_c = chr((ord(clear[i]) + ord(key_c)) % 256)
+        enc.append(enc_c)
+    return base64.urlsafe_b64encode("".join(enc))
+
+
+def decode(key, enc):
+    dec = []
+    enc = base64.urlsafe_b64decode(enc)
+    for i in range(len(enc)):
+        key_c = key[i % len(key)]
+        dec_c = chr((256 + ord(enc[i]) - ord(key_c)) % 256)
+        dec.append(dec_c)
+    return "".join(dec)
+
+
 def send_email_notifications(site):
     logger.info('Sending emails... START.')
+    test_clear = "ghitab"
+    encoded = encode(SECRET_KEY_DEMO, test_clear)
+    logger.info('Encoded: %s', encoded)
+    decoded = decode(SECRET_KEY_DEMO, encoded)
+    logger.info('Decoded: %s', decoded)
     logger.info('Sending emails... STOP.')
     return True
 
