bede677c3f5e93b2da29da680da85d85f0b098d6
Petchesi-Iulian <iamnotacat12@gmail.com>
2019-02-13 12:35:54 +0200

    Use creation date as issued for files at rdf export + upgrade step to trigger pingcr on file objects.

--- land/copernicus/content/content/marshaller.py
+++ land/copernicus/content/content/marshaller.py
@@ -1,9 +1,44 @@
 """ Custom overrides for eea.rdfmarshaller
 """
 
+from zope.component import adapts
+from zope.interface import implements
+
 from eea.rdfmarshaller.archetypes.fields import ShortenHTMLField2Surf
+from eea.rdfmarshaller.interfaces import ISurfResourceModifier, ISurfSession
+from plone.app.blob.interfaces import IATBlob
+from Products.ATContentTypes.interfaces.file import IATFile
+
+
+import rdflib
+import pytz
 
 
 class LandItemDescription(ShortenHTMLField2Surf):
     sentences = 1
     alternate_field = 'dataResourceAbstract'
+
+
+class IssuedFieldModifier(object):
+    """Add publishing information to rdf export
+    """
+    implements(ISurfResourceModifier)
+    adapts(IATFile)
+
+    def __init__(self, context):
+        self.context = context
+
+    def run(self, resource, *args, **kwds):
+        """Change the rdf resource to include issued term
+        """
+        if not hasattr(self.context, 'created'):
+            return
+
+        value = self.context.created().utcdatetime()
+
+        timezone = pytz.timezone('UTC')
+        utc_date = timezone.localize(value)
+        value = rdflib.term.Literal(utc_date,
+                                    datatype=rdflib.term.URIRef(u'http://www.w3.org/2001/XMLSchema#dateTime'
+                                    ))
+        setattr(resource, "dcterms_issued", value)
---
+++ land/copernicus/content/upgrades/evolve36.py
@@ -0,0 +1,45 @@
+from eea.rdfmarshaller.actions.pingcr import ping_CRSDS
+from Products.CMFCore.utils import getToolByName
+from plone import api
+import logging
+
+
+logger = logging.getLogger('land.copernicus.content')
+
+
+def do_ping():
+    site = api.portal.get()
+    cat = getToolByName(site, 'portal_catalog')
+
+    query = {
+        'portal_type': 'File'
+    }
+    results=cat.searchResults(query)
+    logger.info("Found %s objects " % len(results))
+
+    count = 0
+    options = {}
+    options['create'] = False
+    options['service_to_ping'] = 'http://semantic.eea.europa.eu/ping'
+    for res in results:
+        context = res.getObject()
+        url = res.getURL()
+        options['obj_url'] = url + '/@@rdf'
+
+        logger.info("Pinging: %s", url)
+        ping_CRSDS(context, options)
+        logger.info("Finished pinging: %s", url)
+
+        count += 1
+        if count % 100 == 0:
+            logger.info('Went through %s brains' % count)
+
+    logger.info('Finished pinging all brains')
+
+
+def run(_):
+    logger.info(
+        "Pinging objects belonging to the File content type.")
+
+    do_ping()
+    logger.info('Success!')
