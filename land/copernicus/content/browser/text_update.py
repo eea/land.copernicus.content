3024f5844483327a7042ad0b26ee297200797749
Ghiță Bizău <ghita_bizau@yahoo.com>
2019-02-27 15:47:23 +0200

    [refs #102884] Text update: prepare configuration for Czechia and North Macedonia.

--- land/copernicus/content/browser/text_update.py
+++ land/copernicus/content/browser/text_update.py
@@ -5,9 +5,49 @@ import logging
 
 logger = logging.getLogger('land.copernicus.content')
 
+""" The list of texts to be updated. Usage:
+{
+    'old': "the text to be replaced",
+    'old_not': ["don't update if this is found", "or this"],
+    'new': "the new text"
+},"""
 
-def replace_texts(site, old, new):
-    logger.info("Replaced {0} with {1}".format(old, new))
+TEXTS = [
+    {
+        'old': "Czech Republic",
+        'old_not': [],
+        'new': "Czechia"
+    },
+    {
+        'old': "the former Yugoslavian Republic of Macedonia",
+        'old_not': [],
+        'new': "North Macedonia"
+    },
+    {
+        'old': "Macedonia",
+        'old_not': [
+            'North Macedonia',
+            'Republic of Macedonia',
+            'Macedonia the former'
+            ],
+        'new': "North Macedonia"
+    },
+    {
+        'old': "FYROM",
+        'old_not': [],
+        'new': "North Macedonia"
+    },
+    {
+        'old': "Macedonia the former Yugoslavian Republic of",
+        'old_not': [],
+        'new': "North Macedonia"
+    },
+]
+
+
+def replace_texts(site, old, old_not, new):
+    logger.info("Replaced {0} WITH {1} if NOT found: {2}".format(
+        old, new, " OR ".join(old_not)))
 
 
 class TextUpdateView(BrowserView):
@@ -18,6 +58,8 @@ class TextUpdateView(BrowserView):
 
     def __call__(self):
         site = self.context.portal_url.getPortalObject()
-        replace_texts(site, "demo", "demo")
+        for change in TEXTS:
+            replace_texts(
+                site, change['old'], change['old_not'], change['new'])
 
         return self.render()
