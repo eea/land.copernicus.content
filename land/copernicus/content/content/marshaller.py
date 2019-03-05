34b310822c0677e4bf25c1a43a20d2f6438d8f85
Petchesi-Iulian <iamnotacat12@gmail.com>
2019-03-05 14:49:02 +0200

    Refs #99788 Added old search page override + use creation date for images instead of issued.

--- land/copernicus/content/content/marshaller.py
+++ land/copernicus/content/content/marshaller.py
@@ -8,7 +8,7 @@ from eea.rdfmarshaller.archetypes.fields import ShortenHTMLField2Surf
 from eea.rdfmarshaller.interfaces import ISurfResourceModifier, ISurfSession
 from plone.app.blob.interfaces import IATBlob
 from Products.ATContentTypes.interfaces.file import IATFile
-
+from Products.PloneHelpCenter.interfaces import IHelpCenterFolder
 
 import rdflib
 import pytz
@@ -23,7 +23,7 @@ class IssuedFieldModifier(object):
     """Add publishing information to rdf export
     """
     implements(ISurfResourceModifier)
-    adapts(IATFile)
+    adapts(IATBlob)
 
     def __init__(self, context):
         self.context = context
@@ -31,6 +31,37 @@ class IssuedFieldModifier(object):
     def run(self, resource, *args, **kwds):
         """Change the rdf resource to include issued term
         """
+        if hasattr(self.context, 'issued') and self.context.get('issued', None):
+            return
+
+        if not hasattr(self.context, 'created'):
+            return
+
+        value = self.context.created().utcdatetime()
+
+        timezone = pytz.timezone('UTC')
+        utc_date = timezone.localize(value)
+        value = rdflib.term.Literal(utc_date,
+                                    datatype=rdflib.term.URIRef(u'http://www.w3.org/2001/XMLSchema#dateTime'
+                                    ))
+        setattr(resource, "dcterms_issued", value)
+
+
+class HelpFolderFieldModifier(object):
+    """Add publishing information to rdf export
+    """
+    implements(ISurfResourceModifier)
+    adapts(IHelpCenterFolder)
+
+    def __init__(self, context):
+        self.context = context
+
+    def run(self, resource, *args, **kwds):
+        """Change the rdf resource to include issued term
+        """
+        if hasattr(self.context, 'issued') and self.context.get('issued', None):
+            return
+
         if not hasattr(self.context, 'created'):
             return
 
---
+++ land/copernicus/content/upgrades/evolve37.py
@@ -0,0 +1,44 @@
+from eea.rdfmarshaller.actions.pingcr import ping_CRSDS
+from Products.CMFCore.utils import getToolByName
+from plone import api
+import logging
+
+
+logger = logging.getLogger('land.copernicus.content')
+
+
+def do_ping():
+    site = api.portal.get()
+    cat = getToolByName(site, 'portal_catalog')
+
+    query = {
+        'portal_type': ['File', 'Image', 'HelpCenterFAQFolder']
+    }
+    results=cat.searchResults(query)
+    logger.info("Found %s objects " % len(results))
+    count = 0
+    options = {}
+    options['create'] = False
+    options['service_to_ping'] = 'http://semantic.eea.europa.eu/ping'
+    for res in results:
+        context = res.getObject()
+        url = res.getURL()
+        options['obj_url'] = url + '/@@rdf'
+
+        logger.info("Pinging: %s", url)
+        ping_CRSDS(context, options)
+        logger.info("Finished pinging: %s", url)
+
+        count += 1
+        if count % 100 == 0:
+            logger.info('Went through %s brains' % count)
+
+    logger.info('Finished pinging all brains')
+
+
+def run(_):
+    logger.info(
+        "Pinging objects belonging to the File/Image content type.")
+
+    do_ping()
+    logger.info('Success!')
