d92aa9d4c68c46de1f8bb3df7a9c97f6b9d9530c
Ghiță Bizău <ghita_bizau@yahoo.com>
2019-03-06 13:02:17 +0200

    [refs #101742] WIP users clean tool.

--- land/copernicus/content/browser/users_clean.py
+++ land/copernicus/content/browser/users_clean.py
@@ -1,3 +1,5 @@
+# from DateTime import DateTime
+from Products.CMFCore.utils import getToolByName
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone import api
@@ -7,12 +9,57 @@ import logging
 logger = logging.getLogger('land.copernicus.content')
 
 
+def get_users_for_email(site, email):
+    md = getToolByName(site, 'portal_memberdata')
+
+    _members = md._members
+    _properties = site['acl_users']['mutable_properties']._storage
+
+    result = []
+    for idx, user_id in enumerate(_members.iterkeys()):
+        user_properties = _properties.get(user_id, dict())
+        user_email = user_properties.get('email', '')
+
+        if user_email == email:
+            result.append(user_id)
+
+    return result
+
+
 def users_get_details(site, emails):
     html_logs = ""
 
+    # md = getToolByName(site, 'portal_memberdata')
+
+    # _members = md._members
+    # _properties = site['acl_users']['mutable_properties']._storage
+    # _never_active = DateTime("2010/01/01")
+
+    users = []
     for email in emails:
-        logger.info("{0}".format(emails))
-        html_logs += "<p>Details for {0}</p>".format(email)
+        users += get_users_for_email(email)
+
+        # user_id = "ghitab"
+        # user_properties = _properties.get(user_id, dict())
+        # user_member_data = _members.get(user_id)
+
+        # import pdb; pdb.set_trace()
+        #
+        # if user_member_data is not None:
+        #     active_last = user_properties.get('last_login_time')
+        #     active_from = user_member_data.bobobase_modification_time()
+        #
+        # was_active = True
+        # if active_last is not None and active_from is not None:
+        #     if active_last < _never_active:
+        #         # NEVER USED
+        #         # A lot of accounts have 2000/01/01 as last login.
+        #         # This means the account was created but never used.
+        #         was_active = False
+
+    for user_id in users:
+        logger.info("{0}".format(user_id))
+        html_logs += "<p>Details for {0}</p>".format(user_id)
 
     return html_logs
 
