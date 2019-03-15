babe8d6ca2be72d6cefce51bcf06fe2a5c8232b8
Ghiță Bizău <ghita_bizau@yahoo.com>
2019-03-15 12:04:02 +0200

    [refs #102884] Simplify code.

--- land/copernicus/content/upgrades/evolve38.py
+++ land/copernicus/content/upgrades/evolve38.py
@@ -23,47 +23,19 @@ def do_migration(landitem):
             categories = landfile.fileCategories
             country = dict(categories).get(KEY, "")
 
-            # North Macedonia -------------------------------------------------
-            new_country = "North Macedonia"
+            to_be_updated = False
             if ("the former Yugoslavian Republic of Macedonia" in country) or (
                 "Macedonia" in country) or ("FYROM" in country) or (
                     "Macedonia the former Yugoslavian Republic of" in country):
 
-                res = [replace(x, y, new_country) for x, y in categories]
-                # lfa.edit(
-                #     landfile.title, title=landfile.title,
-                #     description=landfile.description,
-                #     remoteUrl=landfile.remoteUrl,
-                #     fileCategories=tuple(res)
-                # )
-
-                new_c = dict(lfa.get(landfile.title).fileCategories).get(
-                        "Country", "")
-
-                if new_c == new_country:
-                    logger.info(
-                        "[OK] Migrated: {0} [{1}]: {2} -> {3}".format(
-                            landitem.absolute_url(1),
-                            landfile.title,
-                            country,
-                            new_c
-                        )
-                    )
-                else:
-                    logger.info(
-                        "[??] Migrated: {0} [{1}]: {2} -> {3}".format(
-                            landitem.absolute_url(1),
-                            landfile.title,
-                            country,
-                            new_c
-                        )
-                    )
-
-            # Czechia ---------------------------------------------------------
-            new_country = "Czechia"
+                new_country = "North Macedonia"
+                to_be_updated = True
 
             if "Czech Republic" in country:
+                new_country = "Czechia"
+                to_be_updated = True
 
+            if to_be_updated is True:
                 res = [replace(x, y, new_country) for x, y in categories]
                 # lfa.edit(
                 #     landfile.title, title=landfile.title,
