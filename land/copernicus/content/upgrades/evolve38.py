dbfded9ee5e9f41e67e65965239eb87b6eb71b97
Ghiță Bizău <ghita_bizau@yahoo.com>
2019-03-15 10:12:21 +0200

    [refs #102884] WIP migration for land files

--- land/copernicus/content/upgrades/evolve38.py
+++ land/copernicus/content/upgrades/evolve38.py
@@ -1,9 +1,21 @@
 import logging
+from plone import api
+from land.copernicus.content.content.api import LandFileApi
+from land.copernicus.content.content.landfile import LandFileStore
 
 
 logger = logging.getLogger('land.copernicus.content')
 
 
+def do_migration(landitem):
+    logger.info('Migrated: %s', landitem.absolute_url(1))
+
+
 def run(_):
-    import pdb; pdb.set_trace()
-    logger.info('Success!')
+    catalog = api.portal.get_tool(name='portal_catalog')
+    landitems = [b.getObject() for b in catalog(portal_type='LandItem')]
+    for landitem in landitems:
+        do_migration(landitem)
+        # tree = landitem.landfiles
+
+        # if not isinstance(tree, LandFileStore):
