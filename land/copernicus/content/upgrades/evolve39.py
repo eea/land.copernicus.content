a886abc5d83500b925cefc238279bf09dc02be71
Ghiță Bizău <ghita_bizau@yahoo.com>
2019-03-29 15:09:29 +0200

    [refs #103555] WIP upgrade step.

--- land/copernicus/content/upgrades/evolve39.py
+++ land/copernicus/content/upgrades/evolve39.py
@@ -17,9 +17,10 @@ def delete_unused_accounts():
     _members = md._members
     _properties = site['acl_users']['mutable_properties']._storage
     _never_active = DateTime("2010/01/01")
+    _new_accounts = DateTime("2019/01/01")
 
+    deleted = 0
     for idx, user_id in enumerate(_members.iterkeys()):
-        print "{0}: {1}".format(idx, user_id)
         user_properties = _properties.get(user_id, dict())
         user_member_data = _members.get(user_id)
 
@@ -28,6 +29,7 @@ def delete_unused_accounts():
             active_from = user_member_data.bobobase_modification_time()
 
             was_active = True
+            is_new_account = True
             if active_last is not None and active_from is not None:
                 if active_last < _never_active:
                     # NEVER USED
@@ -35,9 +37,15 @@ def delete_unused_accounts():
                     # This means the account was created but never used.
                     was_active = False
 
-            if was_active is False:
+            if active_from < _new_accounts:
+                is_new_account = False
+
+            if was_active is False and is_new_account is False:
+                deleted += 1
                 logger.info("WIP Delete {0}: {1} - [{2} - {3}]".format(
                     idx, user_id, active_from, active_last))
+
+    logger.info("Done. {0} deleted accounts.".format(deleted))
 
 
 def run(_):
