f00bc5c20ce385d1653cde4c2f0b8e25f79f73d2
Ghiță Bizău <ghita_bizau@yahoo.com>
2020-02-03 15:01:37 +0200

    [refs #113682] Views: move landitem overview.

--- land/copernicus/content/browser/views.py
+++ land/copernicus/content/browser/views.py
@@ -1,32 +1,4 @@
-from Products.CMFCore.utils import getToolByName
-from Products.CMFPlone.PloneBatch import Batch
-from Products.Five.browser import BrowserView
-import logging
 import subprocess
-
-logger = logging.getLogger('land.copernicus.content')
-
-
-class LandItemsOverview(BrowserView):
-    """ Overview page for LandItems
-    """
-    def __call__(self):
-        """ Render the content item listing.
-        """
-        self.limit = self.request.get('limit', '10')
-        start = self.request.get("b_start", '0')
-        self.contents = self.find_landitems(start)
-
-        return self.index()
-
-    def find_landitems(self, start):
-        context = self.context
-        catalog = getToolByName(context, 'portal_catalog')
-        results = catalog.searchResults({'portal_type': 'LandItem'})
-        results = [result.getObject() for result in results]
-        batch = Batch(results, int(self.limit), int(start), orphan=0)
-
-        return batch
 
 
 def remoteUrl_exists(location):
