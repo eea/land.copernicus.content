3f82d87b8105fa495ae609c16e3b2cbb220d687d
Ghiță Bizău <ghita_bizau@yahoo.com>
2020-04-01 14:32:57 +0300

    [refs #116247] WIP coronavirus news.

--- land/copernicus/content/browser/coronavirus.py
+++ land/copernicus/content/browser/coronavirus.py
@@ -1,3 +1,4 @@
+from Products.CMFPlone.utils import isExpired
 from Products.Five.browser import BrowserView
 
 
@@ -6,4 +7,30 @@ class ExportNews(BrowserView):
     """
 
     def __call__(self):
-        return {"a": "aaa"}
+        catalog = self.context.portal_catalog
+        query = {
+            'portal_type': 'News Item',
+            'sort_on': 'effective',
+            'sort_order': 'descending',
+            'review_state': 'published',
+        }
+
+        # [
+        #     {
+        #         "id":"123",
+        #         "title":"Title",
+        #         "link":"https:\/\/...",
+        #         "image":"https:\/\/...",
+        #         "abstract":"text",
+        #         "tags":["Coronavirus"],
+        #         "created":"2020-04-01T13:10:48+01:00",
+        #         "updated":"2020-04-01T11:34:02+01:00"
+        #     },{...}
+        # ]
+
+        result = [x for x in catalog(**query) if isExpired(x) != 1]
+        covid_news = [
+            x for x in result if 'Coronavirus' in x.Subject or
+            'Covid-19' in x.Subject]
+
+        return covid_news
