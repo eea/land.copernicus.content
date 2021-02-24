3066f11c082e3af12c74d8d68570c6c73dd2ee34
GhitaB <ghitabizau27@gmail.com>
2021-02-24 14:41:51 +0200

    [refs #128791] Add plone.app.dexterity to install_requires.

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
+        'plone.app.dexterity',
         'setuptools',
         'archetypes.schemaextender',
         'lxml',
